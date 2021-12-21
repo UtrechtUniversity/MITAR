a5e8ca8ac3a764260714d4c2e3816636576b50cd
JesseAlderliesten <jessealderliesten@hotmail.com>
2021-12-21 13:10:22 +0100

    Removed use of beepr::beep, as it occasionaly leads to fatal errors, crashing R.

--- multispeciespinnewspecies.R
+++ multispeciespinnewspecies.R
@@ -126,9 +126,6 @@ library(ggplot2)   # to display data and results
 library(rootSolve) # geteqinfo() calls jacobian.full()
 library(TruncatedNormal) # getintmat calls rtnorm()
 # On the pipe operator (%>%), see ?'%>%' and Ch. 18 'Pipes' in Wickham 2017.
-require(beepr, quietly = TRUE) # Optional package. If installed, a sound is
-                               # played when the simulations are finished and
-                               # when plotting is finished.
 
 
 #### Settings and defining parameter space ####
@@ -1322,9 +1319,6 @@ for(index in 1:length(settings)) {
               quote = FALSE, sep = ",", col.names = FALSE)
 }
 
-# If the beepr package is installed, a sound is played
-beepr::beep(2)
-
 
 #### Reading previously saved data from a .csv-file ####
 ## To read data from csv-file, uncomment this section and fill in the 
@@ -1878,6 +1872,3 @@ if(simulateinvasion == TRUE) {
                                "with P of newly\nintroduced species 1"),
              filltype = "continuous", limits = limits)
 }
-
-# If the beepr package is installed, a sound is played
-beepr::beep(2)
