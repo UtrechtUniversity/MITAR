40bc1717d70781acfd75e7d6e51af74c4dbbccfe
JesseAlderliesten <jessealderliesten@hotmail.com>
2025-03-24 09:49:05 +0100

    Use more-detailed parameter set. Added tags to figures. Customize colour palette. Some cleaning up.

--- multispecies_distr.R
+++ multispecies_distr.R
@@ -12,8 +12,7 @@ distr_means_inter <- c(-1e-11, 0, 5e-12)
 distr_sd_inter <- 5e-12
 ub_inter <- Inf
 n_species <- 3
-n_vals <- 1.1e6
-
+n_vals <- 2^23 # is rounded up to power of 2, see ?density
 
 n_sample_intra <- n_species
 n_sample_inter <- n_species * (n_species - 1)
@@ -24,6 +23,11 @@ set.seed(seed = 314, kind = "default", normal.kind = "default", sample.kind = "d
 #### Intraspecies ####
 
 ##### Calculations #####
+# Notes:
+# - In the main script the following code is used to obtain the intraspecies
+#   interaction coefficients:
+#   rtnorm(n = nspecies, mu = selfintmean, sd = selfintsd, lb = -Inf, ub = 0,
+#          method = "invtransfo")
 # To do:
 # - When calculating normalized y normalize by part not truncated? E.g., look
 #   for an y for which TruncatedNormal::norminvp(y, -Inf, Inf) gives ...
@@ -70,9 +74,20 @@ vals_sample_ext$set <- as.factor(vals_sample_ext$set)
 #      ylab = "Density"); grid(); abline(h = 0, col = "darkgrey")
 # The vertical lines can be added using:
 # abline(v = vals_sample_ext$x, col = "darkgrey", lwd = 2, lty = 2)
-labset <- paste0("mean: ", signif(distr_means_intra, 3), ", sd: ", signif(distr_sd_intra, 3))
+labset <- paste0("mean: ", signif(distr_means_intra, 3),
+                 ", sd: ", signif(distr_sd_intra, 3))
 names(labset) <- seq_along(distr_means_intra)
 my_labeller_dens <- labeller(set = labset, .default = label_value)
+
+# To have same colours for the same mean values of the intraspecies and
+# interspecies interaction coefficients
+n_col_max <- max(length(distr_means_intra), length(distr_means_inter))
+if(n_col_max != 3L) {
+  stop("Adjust ordering of palette colours to the changed number of interactions.")
+}
+my_cols <- ((scales::hue_pal()(n_col_max))[c(1, 3, 2)])
+my_cols_intra <- my_cols[seq_along(distr_means_intra)]
+names(my_cols_intra) <- levels(vals_dens_ext$set)
 
 # Notes:
 # - Does geom_text_repel() change the seed of the random number generator?
@@ -81,25 +96,29 @@ my_labeller_dens <- labeller(set = labset, .default = label_value)
 #   labelled, so that text also evades the curve:
 #   geom_label_repel(mapping = aes(label = ifelse(order != "", signif(x, 2), ""),
 #                                  x = x, y = 0.5 * y, col = set),
-#                    data = vals_dens_ext, size = 3, label.size = NA, min.segment.length = 0,
-#                    max.overlaps = 100, show.legend = FALSE, inherit.aes = FALSE)
+#                    data = vals_dens_ext, size = 3, label.size = NA,
+#                    min.segment.length = 0, max.overlaps = 100,
+#                    show.legend = FALSE, inherit.aes = FALSE)
 p_intra <- ggplot(data = vals_dens_ext,
                   aes(x = x, y = y, col = set)) +
-  theme_bw(base_size = 13) +
+  theme_bw(base_size = 15) +
   theme(legend.position = "none",
         axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
         axis.text.y = element_blank()) +
+  scale_colour_manual(values = my_cols_intra) +
   geom_line(size = 0.75) +
   coord_cartesian(xlim = c(-3e-11, 0), expand = FALSE) +
   labs(x = "Intraspecies interaction coefficient",
-       y = "Density (arbitrary units)")
+       y = "Probability density (arbitrary units)",
+       tag = "A")
 p_intra
 ggsave(paste0("distr_intra", DateTimeStamp, ".png"),
+       width = 1650, height = 2675, units = "px", dpi = 300)
+ggsave(paste0(DateTimeStamp, "Fig08A.png"),
        width = 1650, height = 2675, units = "px", dpi = 300)
 
 p_intra_lines <- p_intra +
   facet_wrap(facets = vars(set), ncol = 1, labeller = my_labeller_dens) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
   geom_linerange(mapping = aes(x = x, ymin = -Inf, ymax = Inf, col = set),
                  data = vals_sample_ext, size = 0.75, linetype = 2,
                  show.legend = FALSE, inherit.aes = FALSE)
@@ -128,9 +147,14 @@ ggsave(paste0("distr_intra_labelled", DateTimeStamp, ".png"),
 #### Interspecies ####
 
 ##### Calculations #####
-# To do:
-# - When calculating normalized y normalize by part not truncated? E.g., look
-#   for an y for which TruncatedNormal::norminvp(y, -Inf, Inf) gives ...
+# Notes:
+# - A faster way to obtain the first columns of vals_dens_ext would be
+#   n_out <- 301
+#   p_x <- rep(seq(from = -3e-11, to = 2e-11, length.out = n_out),
+#              each = length(distr_means_inter))
+#   p_y <- dnorm(x = p_x, mean = distr_means_inter, sd = distr_sd_inter)
+#   p_mean <- rep(distr_means_inter, n_out)
+#   p_data <- data.frame(x = p_x, y = p_y, set = p_mean)
 vals_dens_ext <- vector(mode = "list", length = length(distr_means_inter))
 vals_sample_ext <- vector(mode = "list", length = length(distr_means_inter))
 dens_obj_ext <- vector(mode = "list", length = length(distr_means_inter))
@@ -163,7 +187,7 @@ vals_sample_ext <- do.call(rbind, vals_sample_ext)
 vals_sample_ext$set <- as.factor(vals_sample_ext$set)
 
 ##### Plots #####
-# This part can also be plotted in base-R with
+# This part can also be plotted in base-R with:
 # plot(dens_obj_ext[[1]], col = 1, ylim = c(0, max(vals_dens_ext$y)));
 # for(index_plot in 2L:length(dens_obj_ext)) {
 #   lines(dens_obj_ext[[index_plot]], col = index_plot)
@@ -177,26 +201,32 @@ labset <- paste0("mean: ", signif(distr_means_inter, 3))
 names(labset) <- seq_along(distr_means_inter)
 my_labeller_dens <- labeller(set = labset, .default = label_value)
 
+my_cols_inter <- my_cols[seq_along(distr_means_inter)]
+names(my_cols_inter) <- levels(vals_dens_ext$set)
+
+
 # Notes:
 # - See 'Notes' above in the section 'Intraspecies'
-# To do:
-# - See 'To do' above in the section 'Intraspecies'
 p_inter <- ggplot(data = vals_dens_ext,
                   aes(x = x, y = y, col = set)) +
-  theme_bw(base_size = 13) +
+  theme_bw(base_size = 15) +
   theme(legend.position = "none",
         axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
         axis.text.y = element_blank()) +
+  scale_colour_manual(values = my_cols_inter) +
   geom_line(size = 0.75) +
   coord_cartesian(xlim = c(-3e-11, 2e-11), expand = FALSE) +
   labs(x = "Interspecies interaction coefficient",
-       y = "Density (arbitrary units)")
+       y = "Probability density (arbitrary units)",
+       tag = "B")
 p_inter
 ggsave(paste0("distr_inter", DateTimeStamp, ".png"),
        width = 2 * 1650 * 5/6, height = 2675, units = "px", dpi = 300)
+ggsave(paste0(DateTimeStamp, "Fig08B.png"),
+       width = 1650, height = 2675, units = "px", dpi = 300)
 
 p_inter_lines <- p_inter +
-  facet_wrap(facets = vars(set), nrow = 1, labeller = my_labeller_dens) +
+  facet_wrap(facets = vars(set), ncol = 1, labeller = my_labeller_dens) +
   geom_linerange(mapping = aes(x = x, ymin = -Inf, ymax = Inf, col = set),
                  data = vals_sample_ext, size = 0.75, linetype = 2,
                  show.legend = FALSE, inherit.aes = FALSE)
